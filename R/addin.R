19efa32793dc8614bb1e88017d83b8cf617101a2
Matt Dray <mwdray@gmail.com>
2020-08-02 21:26:04 +0100

    Rename to addin_speak(), add addin_print(), re-document

--- R/addin.R
+++ R/addin.R
@@ -1,9 +1,9 @@
-#' Vocalise An R Expression
+#' RStudio Addin: Vocalise An R Expression
 #'
 #' Vocalise selected R expression aloud in English (macOS only).
 #'
 #' @export
-r2eng_vocalise <- function() {
+addin_speak <- function() {
 
   # Get RStudio editor info
   active_doc <- rstudioapi::getActiveDocumentContext()
@@ -18,6 +18,31 @@ r2eng_vocalise <- function() {
 
     # Use the speak() method
     speak(obj)
+
+  }
+
+}
+
+#' RStudio Addin: Print Translated R Expression
+#'
+#' Print the highlighted R expression and English translation
+#'
+#' @export
+addin_print <- function() {
+
+  # Get RStudio editor info
+  active_doc <- rstudioapi::getActiveDocumentContext()
+
+  if (!is.null(active_doc)) {
+
+    # Capture current highlighted expression
+    selected_expr <- active_doc$selection[[1]]$text
+
+    # Capture expression as translated r2eng object
+    obj <- translate_string(selected_expr, speak = FALSE)
+
+    # Use the print() method to print R expression and English sentence
+    print(obj)
 
   }
 
